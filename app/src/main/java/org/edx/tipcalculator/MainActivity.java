1bb7a4760e41fafc1aceef33be5fc45114da58e8
paffman <git2303>
2020-02-02 14:31:54 +0100

    percentage switch added

--- app/src/main/java/org/edx/tipcalculator/MainActivity.java
+++ app/src/main/java/org/edx/tipcalculator/MainActivity.java
@@ -3,12 +3,65 @@ package org.edx.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.etBillAmount)
+    EditText billAmount;
+    @BindView(R.id.tvTipPercent)
+    TextView tvTipPercent;
+    @BindView(R.id.tvTipTotal)
+    TextView tvTipTotal;
+    @BindView(R.id.tvBillTotal)
+    TextView tvBillTotal;
+
+    float percentage = 0;
+    float totalAmount = 0;
+    float finalBillAmount = 0;
+
+    float REGULAR_TIP_PERCENTAGE = 10;
+    float DEFAULT_TIP_PERCENTAGE = 15;
+    float EXCELLENT_TIP_PERCENTAGE = 20;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setTipValues();
+    }
+
+    /**
+     * replace default values with numbers
+     */
+    private void setTipValues() {
+        tvTipPercent.setText(getString(R.string.main_msg_tippercent, percentage));
+        tvTipTotal.setText(getString(R.string.main_msg_tiptotal, totalAmount));
+        tvBillTotal.setText(getString(R.string.main_msg_billtotalresult, finalBillAmount));
+
+    }
+
+    @OnClick({R.id.ibExcellentService, R.id.ibGoodService, R.id.ibRegularService})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ibRegularService:
+                percentage = REGULAR_TIP_PERCENTAGE;
+                break;
+            case R.id.ibGoodService:
+                percentage = DEFAULT_TIP_PERCENTAGE;
+                break;
+            case R.id.ibExcellentService:
+                percentage = EXCELLENT_TIP_PERCENTAGE;
+                break;
+        }
     }
 }
